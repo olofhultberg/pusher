557507373eeeac634e1b486a5a510f5ddfa71cad
Olof Hultberg <olofhultberg@Olofs-MacBook-Pro.local>
2022-05-02 08:45:29 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const Pusher = require("pusher");
+
+const pusher = new Pusher({
+  appId: "1385326",
+  key: "451f9189d7457b091a53",
+  secret: "037982bdae97c87993a3",
+  cluster: "eu",
+  useTLS: true
+});
+
+const app = express();
+
+app.use(cors({
+    origin: ['http://localhost:3000','http://localhost:8080' ]
+}))
+
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+  res.send('Hello 😀')
+})
+
+app.post('/api/messages', async (req, res)=>{
+    await pusher.trigger("chat", "message", {
+        username: req.body.username,
+        message: req.body.message
+      });
+    res.status(200).json([]);
+})
+
+
+console.log('Listening to port 8000');
+
+app.listen(8000)
